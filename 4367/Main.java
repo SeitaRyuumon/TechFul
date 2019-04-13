6c1b497fd4a576ae8ddd80cc3a92c9335dc29b49
supersonico <saita.0521@gmail.com>
2019-04-13 20:08:49 +0900

    new Directory: 4367

---
+++ 4367/Main.java
@@ -0,0 +1,42 @@
+import java.util.Collections;
+import java.util.Scanner;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+class Main {
+	public static void main(String[] args) {
+		ArrayList<Integer> L = new ArrayList<>();
+		int Dist = 0;
+
+		Scanner scan = new Scanner(System.in);
+		// scan.useDelimiter(" |¥n");
+		System.out.println("ok");
+
+		for (int i = 0; i < 3; i++) {
+			L.add(scan.nextInt());
+		}
+		Dist = scan.nextInt();
+		scan.close();
+
+		// 長い順にソート
+		Collections.sort(L, Collections.reverseOrder());
+
+		System.out.println("sort by (DSC):");
+		for (int var : L) {
+			System.out.println(var);
+		}
+		
+		int a = L.get(0) - L.get(1);
+		int b = L.get(1) - L.get(2);
+		if ( a <= Dist && b >= Dist ) {
+			// Do compare <3>
+			System.out.println("3");
+		}
+		if ( a >= Dist && b <= Dist) {
+			// Do compare <1>
+			System.out.println("1");
+		}
+
+	}
+}
